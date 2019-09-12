d9c848901ae42853d1801a513ee96ac5308d6044
Blake Lowrey <blake.lowrey1@gmail.com>
2019-09-12 14:28:45 -0400

    async implemented

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const LanguageService = require('./language-service')
-const { requireAuth } = require('../middleware/jwt-auth')
-const languageRouter = express.Router()
+const express = require('express');
+const LanguageService = require('./language-service');
+const { requireAuth } = require('../middleware/jwt-auth');
+const languageRouter = express.Router();
 const LinkedListHelpers = require('../linkedlist.js');
 
 languageRouter
@@ -11,19 +11,18 @@ languageRouter
       const language = await LanguageService.getUsersLanguage(
         req.app.get('db'),
         req.user.id,
-      )
+      );
 
       if (!language)
         return res.status(404).json({
           error: `You don't have any languages`,
-        })
-
-      req.language = language
-      next()
+        });
+      req.language = language;
+      next();
     } catch (error) {
-      next(error)
+      next(error);
     }
-  })
+  });
 
 languageRouter
   .get('/', async (req, res, next) => {
@@ -31,17 +30,16 @@ languageRouter
       const words = await LanguageService.getLanguageWords(
         req.app.get('db'),
         req.language.id,
-      )
-
+      );
       res.json({
         language: req.language,
         words,
-      })
-      next()
+      });
+      next();
     } catch (error) {
-      next(error)
+      next(error);
     }
-  })
+  });
 
 languageRouter
   .get('/head', async (req, res, next) => {
@@ -61,11 +59,11 @@ languageRouter
           };
           console.log(newLang);
           res.json(newLang);
-        })
-        next();
+        });
+      next();
     }
     catch (error){
-      next(error)
+      next(error);
     }
   });
 
@@ -74,35 +72,34 @@ languageRouter
 languageRouter
   .post('/guess', async (req, res, next) => {
     let {answer} = res.body;
-    LanguageService.getWord(
+    const word = await LanguageService.getWord(
       req.app.get('db'),
-      req.language.head )
-      .then(word=>{
-        let newM = word.memory_value;
-        let correct_count = word.correct_count;
-        let incorrect_count = word.incorrect_count;
-        let total_score = req.language.total_score;
-        let isRight = (answer === word.translation)
-        if(isRight){
-          newM = newM * 2;
-          correct_count++;
-          total_score ++;
-        }
-        else{
-          newM = 1;
-          incorrect_count++;
-          total_score --;
-          await LinkedListHelpers.moveOne(req.app.get('db'), req.user.id, word.id);
-        }
-        await LanguageService.updateWord(req.app.get('db'), word.id, {memory_value : newM, correct_count, incorrect_count});
-        await LanguageService.updateUsersLanguage(req.app.get('db'), req.user.id, {total_score});
-      })
+      req.language.head );
+     
+    let newM = word.memory_value;
+    let correct_count = word.correct_count;
+    let incorrect_count = word.incorrect_count;
+    let total_score = req.language.total_score;
+    let isRight = (answer === word.translation);
+    if(isRight){
+      newM = newM * 2;
+      correct_count++;
+      total_score ++;
+    }
+    else{
+      newM = 1;
+      incorrect_count++;
+      total_score --;
+      await LinkedListHelpers.moveOne(req.app.get('db'), req.user.id, word.id);
+    }
+    await LanguageService.updateWord(req.app.get('db'), word.id, {memory_value : newM, correct_count, incorrect_count});
+    await LanguageService.updateUsersLanguage(req.app.get('db'), req.user.id, {total_score});
 
 
 
 
     // implement me
     res.send('implement me!');
-  })
+  });
 
-module.exports = languageRouter
+module.exports = languageRouter;
--- src/linkedlist.js
+++ src/linkedlist.js
@@ -1,13 +1,13 @@
 const LanguageService = require('./language/language-service.js');
 
 let LinkedListHelpers = {
-  async moveOne(db, user_id , head_id, newIndex = 1){
+  async moveOne(db, user_id, head_id){
     let currNode = await LanguageService.getWord(db, head_id);
     let nextNode = await LanguageService.getWord(db, currNode.next);
     await LanguageService.updateUsersLanguage(db, user_id, {head : nextNode.id});
     await LanguageService.updateWord(db, nextNode.id, {next : currNode.id} );
-    await LanguageService.updateWord(db, currNode.id, {next : nextNode.next} );
+    await LanguageService.updateWord(db, currNode.id, {next : nextNode.next});
   }
-}
+};
 
 export default LinkedListHelpers;
